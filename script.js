e55169343233f42b15aa19810ed92a7b6ba6901f
mariasharmagbay <mariasharmagbay@gmail.com>
2020-06-21 23:35:44 -0400

    Modified JS; updated table.html
    > Did some clean up and refactoring at JS.
    > updated Html and go ahead with changes that me and Foram discussed as Foram have some issues with her machine,

--- script.js
+++ script.js
@@ -1,46 +1,7 @@
 var AllEmp = [];
 
 
-function create_row(empRecord,index) {
-
-    var arrEmpRecord = [];
-    arrEmpRecord =  empRecord;
-
-    var imagepath="";
-    //ISHARA : added condition with job options e.g. Chef; Engineer; Pilot; Police
-    // MSA: ADDED - additional condition which will consider the small and all caps for the Jo title
-    //MSA: get first image path
-    imagepath = getEmpPhoto(arrEmpRecord[2])
-
-    var row = ""
-    row += "<tr id='" + index + "'>\n"
-    row += "<td onclick='remove_person(" + index + ")'><img src='/images/delete.jpg' alt='Delete' style='width:35px;height:30px;'></td>\n"
-    row += "<td><img src='" + imagepath + "' alt='Position' style='width:35px;height:30px;'>"  + "</td>\n"
-    row += "<td>" + arrEmpRecord[0] + "</td>\n"
-    row += "<td>" + arrEmpRecord[1] + "</td>\n"
-    row += "<td>" + arrEmpRecord[2] + "</td>\n"
-    row += "<td>" + arrEmpRecord[3] + "</td>\n"
-    row += "</tr>"
-    return row
-}
-
-
-function refresh_table(){
-    var code_snippet = ""
-
-    for(var i=0; i < AllEmp.length; i++)
-      code_snippet += create_row(AllEmp[i],i)
-  
-    var table = document.querySelector('#my_table')
-    table.innerHTML = code_snippet 
-  }
-
-  function remove_person(index){
-    AllEmp.splice(index, 1)
-    refresh_table()
-  }
-  
-  function onEnterUp(event) {
+function onEnterUp(event) {
     var people =[];
     if (event.code !== 'Enter') return;
     
@@ -67,41 +28,77 @@ function refresh_table(){
 
   }
 
-  function getEmpPhoto(EmpJob){
+  
+function refresh_table(){
+    var code_snippet = ""
+
+    for(var i=0; i < AllEmp.length; i++)
+      code_snippet += create_row(AllEmp[i],i)
+  
+    var table = document.querySelector('#my_table')
+    table.innerHTML = code_snippet 
+  }
+
+function create_row(empRecord,index) {
+
+    var arrEmpRecord = [];
+    arrEmpRecord =  empRecord;
+
+    var imagepath="";
+    imagepath = getEmpPhoto(arrEmpRecord[2])
+
+    var row = ""
+    row += "<tr id='" + index + "'>\n"
+    row += "<td onclick='remove_person(" + index + ")'><img src='/images/delete.jpg' alt='Delete' style='width:35px;height:30px;'></td>\n"
+    row += "<td><img src='" + imagepath + "' alt='Position' style='width:35px;height:30px;'>"  + "</td>\n"
+    row += "<td>" + arrEmpRecord[0] + "</td>\n"
+    row += "<td>" + arrEmpRecord[1] + "</td>\n"
+    row += "<td>" + arrEmpRecord[2] + "</td>\n"
+    row += "<td>" + arrEmpRecord[3] + "</td>\n"
+    row += "</tr>"
+    return row
+}
+
+function remove_person(index){
+    AllEmp.splice(index, 1)
+    refresh_table()
+  }
+
+function getEmpPhoto(EmpJob){
     var path=""
-    if(EmpJob=='Doctor' || EmpJob=='doctor' || EmpJob=='DOCTOR'){
+    if(EmpJob=='Doctor'){
         //alert("Doc")
         return path='/images/Doc.png' 
     }
-    else if(EmpJob=='IT' || EmpJob=='It' || EmpJob=='it') {
+    else if(EmpJob=='IT') {
         //alert("IT")
         return path='/images/IT_job.jpg'
     }
-    else if(EmpJob=='Lawyer' || EmpJob=='lawyer' ||EmpJob=='LAWYER') {
+    else if(EmpJob=='Lawyer') {
         //alert("IT")
         return path='/images/Lawyer.jpg'
     }
-    else if(EmpJob=='Teacher' || EmpJob=='TEACHER' || EmpJob=='teacher') {
+    else if(EmpJob=='Teacher') {
         //alert("IT")
         return path='/images/Teacher.jpg'
     }
-    else if(EmpJob=='Chef' || EmpJob=='CHEF' || EmpJob=='chef') {
+    else if(EmpJob=='Chef') {
         //alert("IT")
         return path='/images/Chef.jpg'
     }
-    else if(EmpJob=='Nurse' || EmpJob=='nurse' || EmpJob=='NURSE') {
+    else if(EmpJob=='Nurse') {
         //alert("IT")
         return path='/images/Nurse.jpg'
     }
-    else if(EmpJob=='Engineer' || EmpJob=='engineer' || EmpJob=='ENGINEER') {
+    else if(EmpJob=='Engineer') {
         //alert("IT")
         return path='/images/Engineer.jpg'
     }
-    else if(EmpJob=='Pilot' || EmpJob=='pilot' || EmpJob=='PILOT') {
+    else if(EmpJob=='Pilot') {
         //alert("IT")
         return path='/images/Pilot.jpg'
     }
-    else if(EmpJob=='Police' || EmpJob=='police' || EmpJob=='POLICE') {
+    else if(EmpJob=='Police') {
         //alert("IT")
         return path='/images/Police.jpg'
     }
